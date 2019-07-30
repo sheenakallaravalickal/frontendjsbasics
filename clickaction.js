f92fc276d4efae0402350d5661305a8e02be4a1b
sheena k sasi <6154sheena@gmail.com>
2019-07-30 21:15:33 +0530

    the basics of frontend js,initial commit

---
+++ clickaction.js
@@ -0,0 +1,4 @@
+function hello() {
+    document.getElementById("nameHeader").innerHTML="web designer"
+
+}
---
+++ formsubmit.js
@@ -0,0 +1,11 @@
+function checkInput() {
+    let fname=document.forms["myForm"]["firstname"].value
+    if (name==''){
+        alert{'false'}
+        return false
+    }
+    else {
+        alert('success')
+    }
+
+}
